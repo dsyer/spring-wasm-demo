4f081d0f63227a9f591062f3706c2c354f017ec6
Dave Syer <dsyer@vmware.com>
2022-03-07 11:28:06 +0000

    Use MessageConverter

--- function/src/main/java/com/example/driver/DemoApplication.java
+++ function/src/main/java/com/example/driver/DemoApplication.java
@@ -1,6 +1,5 @@
 package com.example.driver;
 
-import java.util.Map;
 import java.util.function.Function;
 
 import com.google.protobuf.ByteString;
@@ -10,6 +9,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.messaging.Message;
 import org.springframework.messaging.MessageHeaders;
+import org.springframework.messaging.converter.MessageConverter;
 import org.springframework.messaging.support.MessageBuilder;
 import org.springframework.stereotype.Component;
 import org.springframework.wasm.WasmLoader;
@@ -25,7 +25,7 @@ class DemoApplication {
 }
 
 @Component
-class WASMFunction implements AutoCloseable, Function<Message<byte[]>, Message<byte[]>> {
+class WASMFunction implements AutoCloseable, Function<SpringMessage, SpringMessage> {
 
 	private WasmLoader wasmLoader = new WasmLoader();
 
@@ -34,28 +34,50 @@ class WASMFunction implements AutoCloseable, Function<Message<byte[]>, Message<b
 		this.wasmLoader.close();
 	}
 
-	private SpringMessage message(Message<byte[]> request) {
-		SpringMessage.Builder msg = SpringMessage.newBuilder();
-		MessageHeaders headers = request.getHeaders();
-		for (String key : headers.keySet()) {
-			msg.putHeaders(key, headers.get(key).toString());
+	@Override
+	public SpringMessage apply(SpringMessage input) {
+		try (WasmRunner runner = wasmLoader.runner(new ClassPathResource("message.wasm"))) {
+			return runner == null ? input : runner.call("filter", input, SpringMessage.class);
 		}
-		msg.setPayload(ByteString.copyFrom(request.getPayload()));
-		return msg.build();
 	}
 
-	private Message<byte[]> egassem(SpringMessage input) {
-		MessageBuilder<byte[]> msg = MessageBuilder.withPayload(input.getPayload().toByteArray());
-		Map<String, String> headers = input.getHeadersMap();
-		msg.copyHeaders(headers);
-		return msg.build();
+}
+
+@Component
+class SpringMessageConverter implements MessageConverter {
+
+	@Override
+	public Object fromMessage(Message<?> request, Class<?> type) {
+		byte[] payload = bytes(request.getPayload());
+		if (type.isAssignableFrom(SpringMessage.class) && payload != null) {
+			SpringMessage.Builder msg = SpringMessage.newBuilder();
+			MessageHeaders headers = request.getHeaders();
+			for (String key : headers.keySet()) {
+				msg.putHeaders(key, headers.get(key).toString());
+			}
+			msg.setPayload(ByteString.copyFrom(payload));
+			return msg.build();
+		}
+		return null;
 	}
 
 	@Override
-	public Message<byte[]> apply(Message<byte[]> input) {
-		try (WasmRunner runner = wasmLoader.runner(new ClassPathResource("message.wasm"))) {
-			return runner == null ? input : egassem(runner.call("filter", message(input), SpringMessage.class));
+	public Message<?> toMessage(Object payload, MessageHeaders headers) {
+		SpringMessage input = (SpringMessage) payload;
+		MessageBuilder<byte[]> msg = MessageBuilder.withPayload(input.getPayload().toByteArray());
+		msg.copyHeaders(headers);
+		msg.copyHeaders(input.getHeadersMap());
+		return msg.build();
+	}
+
+	private byte[] bytes(Object payload) {
+		if (payload instanceof String) {
+			return ((String) payload).getBytes();
 		}
+		if (payload instanceof byte[]) {
+			return (byte[]) payload;
+		}
+		return null;
 	}
 
 }
